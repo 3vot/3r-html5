eaac69f1a8f949e188effac908143524b6e1f33b
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-09 16:00:05 -0600

    game dynamcs #1

--- src/javascript/controllers/game/index.js
+++ src/javascript/controllers/game/index.js
@@ -6,18 +6,22 @@ var Game = function( container ){
 	var _this = this;
 	this.lastAudio;
 
-	var sounds = [] ;
+	this.sounds = [] ;
 
 	for (var i = data.sounds.length - 1; i >= 0; i--) {
 		var sound = data.sounds[i];
-		if(sound.level == window.game.level) sounds.push( sound );
+		if(sound.level == window.game.level) this.sounds.push( sound );
 	};
 
-	container.innerHTML = Layout( sounds );
+	container.innerHTML = Layout( this.sounds );
 
-	var gameContainer = container.querySelector(".game");
+	this.gameContainer = container.querySelector(".game");
 
-	gameContainer.onclick = function(e){ _this.playAudio(e); }
+	this.gameContainer.onclick = function(e){ _this.playAudio(e); }
+
+	this.detectSwipe();
+
+	this.currentSoundIndex = 0;
 
 }
 
@@ -32,7 +36,112 @@ Game.prototype.playAudio = function(e){
 		audio.play();
 		this.lastAudio = audio;
 	}
+}
 
+Game.prototype.detectSwipe = function(){
+  this.gameContainer.addEventListener('touchstart', handleTouchStart, false);        
+  this.gameContainer.addEventListener('touchmove', handleTouchMove, false);
+  var _this = this;
+
+  var xDown = null;                                                        
+  var yDown = null;                                                        
+
+  function handleTouchStart( evt ) {                                         
+      xDown = evt.touches[0].clientX;                                      
+      yDown = evt.touches[0].clientY;                                      
+  };                                                
+
+  function handleTouchMove( evt ) {
+  	
+  	var target = evt.target
+ 
+  	function getLeftRight(){
+	  	while( !target.classList.contains("machineSlider") && !target.classList.contains("soundSlider")  ){
+        if( target.classList.contains("container") ) return false
+	  		target = target.parentNode;
+	  	}	
+
+	  	return target;
+		}
+
+		function getUpDown(){
+			while( !target.classList.contains("audioBox")  ){
+  			if( target.classList.contains("container") ) return false
+  			target = target.parentNode;
+  		}	
+  		return target;
+		}
+
+      if ( ! xDown || ! yDown ) {
+          return;
+      }
+
+      var xUp = evt.touches[0].clientX;                                    
+      var yUp = evt.touches[0].clientY;
+
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
+
+      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      		target = getLeftRight();
+          if (!target ) return false;
+
+          var left = target.style.left.replace("px","");
+          left = parseInt(left);
+          left = left || 0;
+
+          if ( xDiff > 0 ) {
+          	left -= 280;
+            var limit = ( _this.sounds.length -1 ) * 280 * -1
+            if( left >= limit ){
+              target.style.left = left + "px";
+              _this.currentSoundIndex++;
+            }
+
+          } else {
+          	left += 280;
+          	if( left < 0){
+              target.style.left = left + "px";
+              _this.currentSoundIndex++;
+            }
+          }
+          
+      } else {
+      		target = getUpDown();
+          var lbl_wrong = document.querySelector(".game .lbl_wrong");
+
+          if( !target ) return false;
+          if ( yDiff > 0 ) {
+            target.classList.remove("active")
+
+          } else { 
+            target.classList.add("active")
+            var sound = _this.sounds[_this.currentSoundIndex];
+            var match = false;
+            if( target.dataset.name == sound.name) match = true;
+            
+            function invalidateMatch(target){
+              setTimeout( function(){
+                lbl_wrong.style.display="none";
+                target.classList.remove("active")
+              },1000)
+
+            }
+
+            if( !match ){
+              lbl_wrong.style.display = "block";
+              invalidateMatch(target);
+            }
+
+           
+            
+          }                                                                 
+      }
+      
+      /* reset values */
+      xDown = null;
+      yDown = null;                                             
+  };
 }
 
 module.exports = Game;
