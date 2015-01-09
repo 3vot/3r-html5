7ebb08b3243ebbd7f8e8eb3ccdc1e6f4bee22fb9
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-09 17:44:04 -0600

    perfect match - remove elements correctly and inverse selections to test more cases

--- src/javascript/controllers/game/index.js
+++ src/javascript/controllers/game/index.js
@@ -5,6 +5,8 @@ var data = require("../../../htdocs/data/game.json")
 var Game = function( container ){
 	var _this = this;
 	
+  this.moveBy = 261;
+
 	this.sounds = [] ;
 
 	for (var i = data.sounds.length - 1; i >= 0; i--) {
@@ -12,13 +14,12 @@ var Game = function( container ){
 		if(sound.level == window.game.level) this.sounds.push( sound );
 	};
 
-	
-
   this.render(container);
 
 	this.gameContainer.onclick = function(e){ _this.playAudio(e); }
 
 	this.currentSoundIndex = 0;
+  this.currentMachineIndex = 0;
 
 }
 
@@ -26,8 +27,6 @@ Game.prototype.render = function(container){
   if(!container) container = this.container.parentNode
 
   container.innerHTML = Layout( this.sounds );
-  if(this.currentSoundIndex >= this.sounds.length)   this.currentSoundIndex = this.sounds.length-1;
-  
   
   this.gameContainer = container.querySelector(".game");
   this.container = this.gameContainer;
@@ -101,22 +100,27 @@ Game.prototype.detectSwipe = function(){
           if (!target ) return false;
 
           var left = target.style.left.replace("px","");
+
           left = parseInt(left);
           left = left || 0;
 
           if ( xDiff > 0 ) {
-          	left -= 280;
-            var limit = ( _this.sounds.length -1 ) * 280 * -1
+          	left -= _this.moveBy;
+            var limit = ( _this.sounds.length -1 ) * _this.moveBy * -1
             if( left >= limit ){
               target.style.left = left + "px";
-              if( target.classList.contains("machineSlider") ) _this.currentSoundIndex++;
+              if( target.classList.contains("soundSlider") ) _this.currentSoundIndex++;
+              if( target.classList.contains("machineSlider") ) _this.currentMachineIndex++;
+
             }
 
           } else {
-          	left += 280;
-          	if( left <= 0){
+          	left += _this.moveBy;
+            console.log(left)
+          	if( left <= 50){
               target.style.left = left + "px";
-              if( target.classList.contains("machineSlider") ) _this.currentSoundIndex--;
+              if( target.classList.contains("soundSlider") ) _this.currentSoundIndex--;
+              if( target.classList.contains("machineSlider") ) _this.currentMachineIndex--;
             }
           }
       } else {
@@ -129,7 +133,7 @@ Game.prototype.detectSwipe = function(){
 
           } else { 
             target.classList.add("active")
-            var sound = _this.sounds[_this.currentSoundIndex];
+            var sound = _this.sounds[_this.currentMachineIndex];
             var match = false;
 
             if( target.dataset.name == sound.name) match = true;
@@ -149,16 +153,18 @@ Game.prototype.detectSwipe = function(){
                 target.remove();
                 var imageEl = _this.gameContainer.querySelector('.imageBox[data-name="'+ sound.name +'"]');
                 imageEl.remove();
-               // _this.render.call(_this);
-               console.log(_this.currentSoundIndex);
-               console.log(_this.sounds.length);
-               if( _this.currentSoundIndex == _this.sounds.length ){
-                _this.currentSoundIndex--;
-                var soundSlider = _this.gameContainer.querySelector(".soundSlider");
-                var machineSlider = _this.gameContainer.querySelector(".machineSlider");
-                soundSlider.style.left = parseInt( soundSlider.style.left.replace("px","") ) + 280 + "px"
-                machineSlider.style.left = parseInt( machineSlider.style.left.replace("px","") ) + 280 + "px"
-               }
+               
+                if( _this.currentSoundIndex == _this.sounds.length){
+                  _this.currentSoundIndex--;
+                  var soundSlider = _this.gameContainer.querySelector(".soundSlider");
+                  soundSlider.style.left = parseInt( soundSlider.style.left.replace("px","") ) + _this.moveBy + "px"
+                }
+
+                if( _this.currentMachineIndex == _this.sounds.length ){                
+                  _this.currentMachineIndex--;
+                  var machineSlider = _this.gameContainer.querySelector(".machineSlider");
+                  machineSlider.style.left = parseInt( machineSlider.style.left.replace("px","") ) + _this.moveBy + "px"
+                }
               },1000)
 
             }
@@ -171,7 +177,7 @@ Game.prototype.detectSwipe = function(){
             else{
               lbl_wrong.style.display = "block";
               lbl_wrong.innerHTML= "Correct! " + _this.sounds.length + " to go."
-              _this.sounds.splice( _this.currentSoundIndex, 1 );
+              _this.sounds.splice( _this.currentMachineIndex, 1 );
               validateMatch(target, sound, _this);
 
             }
