1e6fcee1077857468275f22cd38630b4ccb3db42
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-09 13:09:35 -0600

    fix url write in templates

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+console.log( document.querySelector( "link" ) );
+
+window.game = {};
+window.game.path = document.querySelector('link[rel=template]').href.split("/template.js")[0];
+
+
 var Game = require("./controllers/game");
 
 var container = document.querySelector(".container");
