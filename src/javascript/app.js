846aab70fb7bcefea01b5209e5c2ab0ac7a52a6f
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-09 13:58:49 -0600

    template.js url got rewritten , spliting before hash

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.game = {};
-window.game.path = document.querySelector('link[rel=template]').href.split("/template.js")[0];
+window.game.path = document.querySelector('link[rel=template]').href.split("/template")[0];
 window.game.level = 1;
 
 
