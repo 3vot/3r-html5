d13fad9eefbaa0476281edb96cefac3386382035
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-09 13:18:32 -0600

    test 2 on url rewrit

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-console.log( document.querySelector( "link" ) );
-
 window.game = {};
 window.game.path = document.querySelector('link[rel=template]').href.split("/template.js")[0];
 
+console.log(window.game.path);
 
 var Game = require("./controllers/game");
 
