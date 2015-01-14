aec7d79d71002a29a4189b794c6473f8a43c98ef
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-14 09:56:22 -0600

    no longer will we use sass - using standard css

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,19 +1,14 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var sourcemaps = require('gulp-sourcemaps');
+
 var autoprefixer = require('gulp-autoprefixer');
 var handleErrors = require('../util/handleErrors');
+var concat = require('gulp-concat');
 
 gulp.task('sass', ['images'], function () {
-  return gulp.src('src/sass/*.{sass, scss}')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        sourceComments: 'map',
-        imagePath: '/images'
-    }))
-    .on('error', handleErrors)
-    .pipe(sourcemaps.write())
+  return gulp.src( 'src/css/*.css')
+    .pipe(concat('app.css'))
+    //.on('error', handleErrors)
     .pipe(autoprefixer({browsers: ['last 2 version']}))
     .pipe(gulp.dest('./build/'))
     .pipe(browserSync.reload({stream: true}));
