df0d27529f001cf71712f4ea589259623f135d1a
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-16 17:00:15 -0600

    fix sounds and replace with new ones

--- src/javascript/controllers/win/index.js
+++ src/javascript/controllers/win/index.js
@@ -1,18 +1,17 @@
 var ScreenManager = require("../../managers/screenManager");
 var Layout = require("./layout");
 
-function Retry(container, header, data ){
+function Win(container, header, data ){
 
 	var robot = "";
 	this.data = data;
 	if(data.level ==1) robot = "fax";
-	else if(data.level ==1) robot = "fax";
-	else if(data.level == 3) robot = "fax";
+	else if(data.level == 2) robot = "level2";
+	else if(data.level == 3) robot = "level3";
 
-	container.innerHTML = Layout( { robot: robot } );
+	container.innerHTML = Layout( { robot: robot, level: this.data.level } );
 	
 	this.container = container.querySelector(".win");
-
 	
 	var button = document.querySelector(".btn-next");
 	var share = document.querySelector(".btn-share");
@@ -24,9 +23,10 @@ function Retry(container, header, data ){
 	}
 
 	share.onclick = function(){
-		ScreenManager.emit( "go", { screen: "share", data: { level: _this.data.level } } );
+		if( _this.data.level < 3 ) ScreenManager.emit( "go", { screen: "share", data: { level: _this.data.level } } );
+		else ScreenManager.emit("go", { screen: "home", data: { level: 1 } });
 	}
 
 }
 
-module.exports = Retry;
+module.exports = Win;
