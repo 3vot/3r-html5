c040e09007bb4a8d8dd9a2aea0522a088b25f615
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-01-15 18:27:32 -0600

    play several levels

--- src/javascript/controllers/win/index.js
+++ src/javascript/controllers/win/index.js
@@ -4,16 +4,27 @@ var Layout = require("./layout");
 function Retry(container, header, data ){
 
 	var robot = "";
+	this.data = data;
 	if(data.level ==1) robot = "fax";
+	else if(data.level ==1) robot = "fax";
+	else if(data.level == 3) robot = "fax";
 
 	container.innerHTML = Layout( { robot: robot } );
-	this.container = container;
 	
-	var button = document.querySelector(".btn")
+	this.container = container.querySelector(".win");
 
+	
+	var button = document.querySelector(".btn-next");
+	var share = document.querySelector(".btn-share");
+
+	var _this = this;
 	button.onclick = function(){
-		data.level++;
-		ScreenManager.emit( "go", { screen: "game", data: { level: data.level } } );
+		_this.data.level++;
+		ScreenManager.emit( "go", { screen: "game", data: { level: _this.data.level } } );
+	}
+
+	share.onclick = function(){
+		ScreenManager.emit( "go", { screen: "share", data: { level: _this.data.level } } );
 	}
 
 }
