d37a7995d9bf4d382d19f4401b28debd442427c6
Roberto Rodriguez <roberto@rodriguezartavia.com>
2015-02-14 18:11:46 -0600

    fix depencies and remove images from url suffixing

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -14,8 +14,8 @@ gulp.task('clean', ['build'], function() {
 gulp.task('copy', ['clean'], function() {
     return gulp.src('./build/**')
     .pipe(revall({
-        ignore: [/^\/favicon.ico$/g, /^\/index.html/g, /^\/captions/g, /^\/images\/static/g, /^\/sounds/g, /^\/images\/social/g, /^\/images\/map\/dist/g]//,
-        //prefix: global.previewUrl || p.previewUrl
+        ignore: [/^\/favicon.ico$/g, /^\/index.html/g, /^\/images/g]//,
+        //prefix: global.previewUrl
     }))
     .pipe(gulp.dest('dist'));
 });
